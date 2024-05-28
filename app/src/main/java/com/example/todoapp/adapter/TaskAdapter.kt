c5a90f856d13bbda8b5a434d646b0cd0f6054a59
Yibux <240810@edu.p.lodz.pl>
2024-05-28 07:42:32 +0200

    Addding task layout and TaskAdapter

---
+++ app/src/main/java/com/example/todoapp/adapter/TaskAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.todoapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.TaskLayoutBinding
+import com.example.todoapp.model.Task
+
+class TaskAdapter : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+
+    private lateinit var binding : TaskLayoutBinding
+    inner class ViewHolder : RecyclerView.ViewHolder(binding.root)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        binding = TaskLayoutBinding.inflate(inflater, parent, false)
+        return ViewHolder()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val binding = TaskLayoutBinding.bind(holder.itemView)
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+}
